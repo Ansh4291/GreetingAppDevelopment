501f4204647faaeacc653c81340d5f337db5b6e6
Ansh4291 <anshulrathore1995@gmail.com>
2022-07-09 13:43:21 +0530

    Added:get greeting by id from repository is added.

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -32,7 +32,7 @@ public static final String template = "Hello & Welcome to the greeting app, %s";
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        return new Greeting((int) counter.incrementAndGet(), String.format(template, name));
     }
     @RequestMapping("/welcome")
     public String displayMessage(){
@@ -47,5 +47,9 @@ public static final String template = "Hello & Welcome to the greeting app, %s";
     @PostMapping("/addGreeting")
     public Greeting addGreeting(@RequestBody Greeting greeting){
         return service.saveGreetings(greeting);
+    }
+    @GetMapping("/greetingById/{id}")
+    public Greeting getGreetingById(@PathVariable Integer id){
+        return service.findGreetingById(id);
     }
 }
